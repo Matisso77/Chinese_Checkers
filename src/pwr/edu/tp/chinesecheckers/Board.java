dc3571d618a7731fe3c95be5c91c5adc4e626e0a
Matisso77 <m.jachniakia@gmail.com>
2017-12-03 16:54:36 +0100

    Add board, fill empty fields. TODO: fill players fields

--- src/pwr/edu/tp/chinesecheckers/Board.java
+++ src/pwr/edu/tp/chinesecheckers/Board.java
@@ -1,5 +1,39 @@
 package pwr.edu.tp.chinesecheckers;
 
 public class Board {
-	//TODO
+	public enum Fields {
+		NOTUSED, EMPTY, FULL;
+	}
+	
+	Fields board[][];
+	
+	public Board() {
+		prepareBoard();
+		addPlayesPegs();
+	}
+	
+	private void prepareBoard() {
+		board = new Fields[25][17]; //First field is 0 not 1
+		
+		for(int y = 0; y < board[0].length; y++) {
+			for(int x = 0; x < board.length; x++) {
+				board[x][y] = Fields.NOTUSED;
+			}
+		}
+		
+		int[] fieldsNumberInRow = {1,2,3,4,13,12,11,10,9,10,11,12,13,4,3,2,1}; 
+		for(int y = 0; y < board[0].length; y++) {
+			
+			int tempX = ((board.length + 1) / 2) - (fieldsNumberInRow[y] / 2) * 2 + (y%2);
+			for(int i = 0; i < fieldsNumberInRow[y]; i++) {
+				board[tempX][y] = Fields.EMPTY;
+				tempX+=2;	
+			}
+		}
+	}
+	
+	private void addPlayesPegs() {
+		// TODO MON
+		
+	}
 }
