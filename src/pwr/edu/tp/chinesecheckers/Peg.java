b186c0de10d3fbfe864706cde5cc2ca1b8b91adf
Matisso <m.jachniakia@gmail.com>
2017-12-12 14:05:16 +0100

    Add Pegs into boards, depends of number of players

--- src/pwr/edu/tp/chinesecheckers/Peg.java
+++ src/pwr/edu/tp/chinesecheckers/Peg.java
@@ -1,5 +1,36 @@
 package pwr.edu.tp.chinesecheckers;
 
 public class Peg {
-	//TODO
+
+		
+		private Colors color;
+		private int x, y;
+
+
+		public Peg(Colors color, int x, int y){
+			this.color = color;
+			this.x = x;
+			this.y = y;
+		}
+		
+	
+		public void setX(int x){
+			this.x = x;
+		}
+		
+		public void setY(int y){
+			this.y = y;
+		}
+		
+		public int getX(){
+			return this.x;
+		}
+		
+		public int getY(){
+			return this.y;
+		}
+		
+	
+		
+	
 }
