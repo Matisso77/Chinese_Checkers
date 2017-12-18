21294b24036ac391830e710ccf2754b0470c27ad
Matisso <m.jachniakia@gmail.com>
2017-12-18 22:14:11 +0100

    Add mechanics of game

--- src/pwr/edu/tp/chinesecheckers/Game.java
+++ src/pwr/edu/tp/chinesecheckers/Game.java
@@ -16,8 +16,49 @@ public class Game {
 		return false;
 	}
 
-	public synchronized boolean legalMove(int start, int end, Player player) {
-		// TODO
+	public synchronized boolean legalMove(int startX, int startY, int goalX, int goalY) {
+		        if (!Board.board[startX + 1][startY + 1].equals("EMPTY") && !Board.board[startX + 1][startY + 1].equals("BLOCKED")) {
+		            if (startX + 2 == goalX && startY + 2 == goalY) {
+		                return true;
+		            } else if (Board.board[startX + 2][startY + 2].equals("EMPTY")) {
+		                legalMove(startX + 2, startY + 2, goalX, goalY);
+		            }
+		        }
+		        if (!Board.board[startX + 1][startY - 1].equals("EMPTY") && !Board.board[startX + 1][startY + 1].equals("BLOCKED")) {
+		            if (startX + 2 == goalX && startY - 2 == goalY) {
+		                return true;
+		            } else if (Board.board[startX + 2][startY - 2].equals("EMPTY")) {
+		                legalMove(startX + 2, startY - 2, goalX, goalY);
+		            }
+		        }
+		        if (!Board.board[startX][startY + 2].equals("EMPTY") && !Board.board[startX + 1][startY + 1].equals("BLOCKED")) {
+		            if (startX == goalX && startY + 4 == goalY) {
+		                return true;
+		            } else if (Board.board[startX][startY + 4].equals("EMPTY")) {
+		                legalMove(startX, startY + 4, goalX, goalY);
+		            }
+		        }
+		        if (!Board.board[startX][startY - 2].equals("EMPTY") && !Board.board[startX + 1][startY + 1].equals("BLOCKED")) {
+		            if (startX == goalX && startY - 4 == goalY) {
+		                return true;
+		            } else if (Board.board[startX][startY - 4].equals("EMPTY")) {
+		                legalMove(startX, startY - 4, goalX, goalY);
+		            }
+		        }
+		        if (!Board.board[startX - 1][startY + 1].equals("EMPTY") && !Board.board[startX + 1][startY + 1].equals("BLOCKED")) {
+		            if (startX - 2 == goalX && startY + 2 == goalY) {
+		                return true;
+		            } else if (Board.board[startX - 2][startY + 2].equals("EMPTY")) {
+		                legalMove(startX - 2, startY + 2, goalX, goalY);
+		            }
+		        }
+		        if (!Board.board[startX - 1][startY - 1].equals("EMPTY") && !Board.board[startX + 1][startY + 1].equals("BLOCKED")) {
+		            if (startX - 2 == goalX && startY - 2 == goalY) {
+		                return true;
+		            } else if (Board.board[startX - 2][startY - 2].equals("EMPTY")) {
+		                legalMove(startX - 2, startY - 2, goalX, goalY);
+		            }
+		        }
 		return false;
 	}
 
