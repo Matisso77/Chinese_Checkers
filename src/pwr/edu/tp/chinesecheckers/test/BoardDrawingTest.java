ce864353912e9773bc5f4d1264ca1b5671517346
drzazga3009 <bdrzazga3009@gmail.com>
2017-12-19 00:26:43 +0100

    new, experimental approach to board code, board drawing

---
+++ src/pwr/edu/tp/chinesecheckers/test/BoardDrawingTest.java
@@ -0,0 +1,32 @@
+package pwr.edu.tp.chinesecheckers.test;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+
+import pwr.edu.tp.chinesecheckers.AltBoard;
+
+public class BoardDrawingTest {
+	public static void main(String[] args) throws Exception {
+		JFrame frame = new JFrame("Board Drawing Test");
+		AltBoard drawingArea = new AltBoard(6);
+		frame.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		drawingArea.setBackground(Color.white);
+		drawingArea.setBorder(BorderFactory.createLineBorder(Color.black));
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 1.0;
+		c.weighty = 1.0;
+		c.fill = GridBagConstraints.BOTH;
+		frame.add(drawingArea, c);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(410, 240);
+		frame.setVisible(true);
+		frame.setResizable(true);
+		drawingArea.repaint();
+	}
+}
