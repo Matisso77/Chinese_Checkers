7790c811827091cbb38d0a4f1c1c253a4f8b8887
drzazga3009 <bdrzazga3009@gmail.com>
2017-12-30 14:09:52 +0100

    fixed legalMove test

--- src/pwr/edu/tp/chinesecheckers/test/LegalMoveTest.java
+++ src/pwr/edu/tp/chinesecheckers/test/LegalMoveTest.java
@@ -6,66 +6,25 @@ import java.awt.Color;
 
 import org.junit.Test;
 
+import pwr.edu.tp.chinesecheckers.BotBlue;
 import pwr.edu.tp.chinesecheckers.Game;
 import pwr.edu.tp.chinesecheckers.Player;
 
-public class LegalMoveTest extends Player {
+public class LegalMoveTest {
+	Game game = new Game(6);
+	Game game2 = new Game(2);
+	Game game3 = new Game(3);
+	Game game4 = new Game(4);
 
-
-
-	 Game game = new Game(6);
-	 Game game2 = new Game(2);
-	 Game game3 = new Game(3);
-	 Game game4 = new Game(4);
 	@Test
 	public void test1() {
-	
-		boolean ans= false;
+		Player dummyPlayer = new BotBlue();
+		dummyPlayer.setColor(Color.BLUE, "BLUE");
+		
+		boolean ans = false;
 		System.out.println(game.board.board[12][6].getRGB() == Color.GRAY.getRGB());
-		
-		assertEquals(game.legalMove3(11,3,13,4,this,Color.BLUE), ans);
+		assertEquals(game.legalMove3(11, 3, 13, 4, dummyPlayer, Color.BLUE), ans);
 	}
 
-	@Override
-	public void setColor(Color color, String colorS) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void otherPlayerDone(String s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Boolean sendBoard(Color[][] board) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void yourMove() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void end() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void youFinished() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 }
