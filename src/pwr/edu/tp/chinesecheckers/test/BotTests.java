4d33d9a7c251bb03514d0fc3ca5fc2d149db4a3b
drzazga3009 <bdrzazga3009@gmail.com>
2017-12-30 14:13:41 +0100

    fixed bot test

--- src/pwr/edu/tp/chinesecheckers/test/BotTests.java
+++ src/pwr/edu/tp/chinesecheckers/test/BotTests.java
@@ -2,21 +2,20 @@ package pwr.edu.tp.chinesecheckers.test;
 
 import static org.junit.Assert.*;
 
-
-
+import java.awt.Color;
 import org.junit.Test;
-
 import pwr.edu.tp.chinesecheckers.BotBlack;
-//import pwr.edu.tp.chinesecheckers.Game;
+import pwr.edu.tp.chinesecheckers.Game;
 
 public class BotTests {
 
 	@Test
 	public void test() {
+		Game game = new Game(6);
 		BotBlack botBlack = new BotBlack();
-		//botBlack.Game game = new Game(6);
+		botBlack.setColor(Color.BLACK, "BLACK");
+		botBlack.setGame(game);
 		botBlack.searchField();
 		assertNotEquals(botBlack.goalMoves[0], 1);
 	}
-
 }
